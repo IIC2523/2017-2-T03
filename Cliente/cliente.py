e0ca3cbdd00fcea1ac705107addd6d21396ac584
FranLucchini <flucchini@uc.cl>
2017-11-16 13:27:11 -0300

    ready

--- Cliente/cliente.py
+++
@@ -1,9 +0,0 @@
-import sys
-import socket
-from mpi4py import MPI
-
-def main():
-    print(socket.gethostname())
-
-if __name__ == "__main__":
-    main()
--- Nodo/nodo.py
+++ Nodo/nodo.py
@@ -2,8 +2,16 @@ import sys
 import socket
 from mpi4py import MPI
 
+
 def main():
-    print(socket.gethostname())
+    print("Calcular"+sys.argv[1]+"^"+sys.argv[2]+" mod "+sys.argv[3]+", con "+sys.argv[4]+" nodos corruptos")
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    if rank == 0:
+        data = {'a': 7, 'b': 3.14}
+        comm.send(data, dest=1, tag=11)
+    elif rank == 1:
+        data = comm.recv(source=0, tag=11)
 
 if __name__ == "__main__":
     main()
